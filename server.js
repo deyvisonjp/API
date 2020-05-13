78f6848a23eb062b1ba95c6df855ab8640d980f4
Deyvison J Paula <deyvisonjp@id.uff.br>
2020-05-13 19:58:50 -0300

    Conceitos aprendidos

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const data = require("./data.json");
 
 app.use(express.json());
 
+// Exibe todos
 app.get("/clients", (req, res) => {
    res.json(data);
 });
 
+// Exibe Apenas um cliente pelo param id
 app.get("/clients/:id", (req, res) => {
    const { id } = req.params;
    const client = data.find(cli => cli.id == id);
@@ -18,9 +20,34 @@ app.get("/clients/:id", (req, res) => {
    res.json(client);
 });
 
-app.post("/clients", (req, res) => {});
-app.put("/clients/:id", (req, res) => {});
-app.delete("/clients/:id", (req, res) => {});
+// Salvar um novo cliente
+app.post("/clients", (req, res) => {
+   const { name, email } = req.body;
+
+   //function save
+
+   res.json({ name,  email });
+});
+
+// Atualizar
+app.put("/clients/:id", (req, res) => {
+   const { id } = req.params;
+   const client = data.find(cli => cli.id == id);
+
+   if(!client) return res.status(204).json();
+
+   const { name } = req.body;
+   client.name = name;
+   res.json(client);
+});
+
+app.delete("/clients/:id", (req, res) => {
+   const { id } = req.params;
+   const clientsFilters = data.filter(client => client.id != id);
+
+   res.json(clientsFilters);
+
+});
 
 app.listen(3333, () => {
    console.log('Server is running');
